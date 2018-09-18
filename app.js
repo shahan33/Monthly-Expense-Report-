bc6a26c5f72c7e6ee07f028c82bf1deee02c434a
shahan butt <shahanbutt580@gmail.com>
2018-09-18 15:06:58 +0500

    changes are made

--- app.js
+++ app.js
@@ -21,12 +21,23 @@
 //console.log(salary);
 //document.getElementById('sal').value=salary;
 
+function clickMe(){
+    document.querySelector('#buttonClick').click();
+
+}
 
 
+function getvalue(){
+    var a= document.getElementById('getsalary').value;
+    //document.getElementById('sal').value=a;
+    document.getElementById('sal').innerHTML=a;
+   console.log(a);
+   document.getElementById('SalaryForm').reset();
+      
+}
 
 
-
-
+//$('sal').innerHTML=getvalue();
 
 
 
@@ -37,21 +48,96 @@ function Adding(event){
 //document.getElementById("myform").reset();
 
 
-var table = document.getElementById("table");
-var newRow = table.insertRow(table.length);
-var cell1 = newRow.insertCell(0);
-var cell2 = newRow.insertCell(1);
-var exp  = document.getElementById("exp").value;
-var amount = document.getElementById("amount").value;
+// var table = document.getElementById("table");
+// var newRow = table.insertRow(table.length);
+// var cell1 = newRow.insertCell(0);
+// var cell2 = newRow.insertCell(1);
+// var exp  = document.getElementById("exp").value;
+// var amount = document.getElementById("amount").value;
 
-    cell1.innerHTML = exp;
-    cell2.innerHTML = amount;
+//     cell1.innerHTML = exp;
+//     cell2.innerHTML = amount;
     
-    console.log("row is added sucessfuly");
-    document.getElementById('myform').reset();
+//     console.log("row is added sucessfuly");
+//     document.getElementById('myform').reset();
+
+
+
+
+
+
+
+    //var productName = document.getElementById('exp').value;
+    //var amount = document.getElementById('amount').value;
+
+
+
+
+    var exp  = document.getElementById("exp").value;
+    var amount = document.getElementById("amount").value;
+
+   console.log(amount);
+        /*if((exp=='') && (amount > 0)){
+        document.getElementById('expp').innerHTML = " ** Plese fill the field";
+        document.getElementById('amou').innerHTML =" ** Please enter the amount";
+        }else {
+        var table = document.getElementById("table");
+        var newRow = table.insertRow(table.length);
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        cell1.innerHTML = exp;
+        cell2.innerHTML = amount;
+        console.log("row is added sucessfuly");
+        document.getElementById('myform').reset();
+
+        document.getElementById('expp').innerHTML="";
+        document.getElementById('amou').innerHTML="";
+     }*/
+
+     if(exp && amount){
+        var table = document.getElementById("table");
+        var newRow = table.insertRow(table.length);
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        cell1.innerHTML = exp;
+        cell2.innerHTML = amount;
+        console.log("row is added sucessfuly");
+        document.getElementById('myform').reset();
+
+        document.getElementById('expp').innerHTML="";
+        document.getElementById('amou').innerHTML="";
+     } else{
+         if(exp){
+            document.getElementById('expp').innerHTML = "";
+         } else {
+            document.getElementById('expp').innerHTML = " ** Plese fill the field";
+         }
+         if(amount){
+            document.getElementById('amou').innerHTML ="";
+         } else{
+            document.getElementById('amou').innerHTML =" ** Please enter the amount";
+         }
+        
+        
+     }
+
+
+}
+
+   
+      
+
+       //return false;
+   
+
+ 
+
+
+
+
     
     //document.getElementById("submit").disabled = true;
-}
+
 //var one;
 function sumofexpen(){
     var table1 = document.getElementById("table"), sumval =0;
@@ -68,10 +154,16 @@ function sumofexpen(){
 
 
 function calc(){
-    var test1 = document.getElementById('sal').value;
+    var test1=document.getElementById('sal').innerHTML;
+     console.log(test1);
     var test2 = sumofexpen();
     //var test3 = test1+test2;
 
     var FinalResult= parseInt(document.getElementById("result").value=test1-test2);
     console.log("your final result is :"+FinalResult);
 }
+
+
+
+
+
